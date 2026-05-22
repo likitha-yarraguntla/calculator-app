aa1775fbf222486b9c6d843e0f3a402e4284ca62
likitha-yarraguntla <likithayarraguntla.2006@gmail.com>
2026-05-22 18:54:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+
+    display.value += value;
+
+    try{
+        display.value = eval(display.value);
+    }
+
+    catch{
+
+    }
+
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value);
+    }
+
+    catch{
+        display.value = "Error";
+    }
+
+}
+document.addEventListener("keydown", function(event){
+
+    let key = event.key;
+
+    if(
+        key >= "0" && key <= "9" ||
+        key == "+" ||
+        key == "-" ||
+        key == "*" ||
+        key == "/" ||
+        key == "."
+    ){
+        appendValue(key);
+    }
+
+    else if(key == "Enter"){
+        calculate();
+    }
+
+    else if(key == "Backspace"){
+        display.value = display.value.slice(0, -1);
+    }
+
+});
+function deleteLast(){
+
+    display.value = display.value.slice(0, -1);
+
+}
